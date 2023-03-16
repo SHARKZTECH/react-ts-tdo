0e6d707f61cb8196d748699eb93a4c9235db06a4
sharkz <mosenjemudogoz@gmail.com>
2023-03-16 20:20:28 +0300

    Complete drag

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,14 +1,17 @@
 import React,{useEffect, useState} from 'react'
 import {MdCheck, MdDelete, MdEdit} from "react-icons/md"
+import { DragDropContext, Draggable, Droppable,DropResult } from 'react-beautiful-dnd';
+
 import { Todo } from './Todo';
 
 type Props={
    todo:Todo,
    todos:Array<Todo>,
-   setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
+   setTodos:React.Dispatch<React.SetStateAction<Todo[]>>,
+   idx:number
 }
 
-const TodoItem:React.FC<Props> = ({todo,todos,setTodos}) => {
+const TodoItem:React.FC<Props> = ({todo,todos,setTodos,idx}) => {
     const [edit,setEdit]=useState<boolean>(false);
     const [text,setText]=useState<string>("");
 
@@ -27,18 +30,24 @@ const TodoItem:React.FC<Props> = ({todo,todos,setTodos}) => {
         setTodos(todos.map(t=> t.id === id ? {...t,isDone:!t.isDone} : t));
     }
   return (
-    <div className={todo.isDone ? 'todo_item_container comp' : 'todo_item_container'}>
-    {edit? (
-        <input value={text} onChange={(e)=>setText(e.target.value)} />
-    ):(
-        <p style={{color:'whitesmoke'}}>{todo.todo}</p>
-    )}
-    <div>
-    <MdEdit size={30} onClick={()=>handleEdit(todo.id)}/>
-    <MdDelete size={30} color="red" onClick={()=>handleDelete(todo.id)}/>
-    <MdCheck size={30} onClick={()=>handleComplte(todo.id)} />
-    </div>
-</div>
+    <Draggable draggableId={todo?.id.toString()} index={idx}>
+        {provided=>(
+        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <div className={todo.isDone ? 'todo_item_container comp' : 'todo_item_container'}>
+            {edit? (
+                <input value={text} onChange={(e)=>setText(e.target.value)} />
+            ):(
+                <p style={{color:'whitesmoke'}}>{todo.todo}</p>
+            )}
+            <div>
+            <MdEdit size={30} onClick={()=>handleEdit(todo.id)}/>
+            <MdDelete size={30} color="red" onClick={()=>handleDelete(todo.id)}/>
+            <MdCheck size={30} onClick={()=>handleComplte(todo.id)} />
+            </div>
+            </div>
+        </div>
+        )}
+   </Draggable>
   )
 }
 
